a91e5a1777bbba8d9c3d05fb3ab1d3c4b69a632a
jamesliao <JamesCoding888@hotmail.com>
2022-05-11 18:31:45 +0800

    SpringMVC20220510

--- src/main/java/com/mvc/controller/Hello.java
+++ src/main/java/com/mvc/controller/Hello.java
@@ -1,8 +1,12 @@
 package com.mvc.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Hello {
-	public static void main(String[] args) {
-		System.out.println("h");
+	@RequestMapping(value = "/greet")
+	@ResponseBody()
+	public String greet() {
+		return "Hello to Spring MVC";
 	}
 }
