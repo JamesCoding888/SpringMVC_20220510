f4268493bc9384f0cc9ae6d5b50f6a95e9148840
jamesliao <JamesCoding888@hotmail.com>
2022-05-17 18:49:57 +0800

    springmvc20220510

--- src/main/java/com/mvc/controller/Hello.java
+++ src/main/java/com/mvc/controller/Hello.java
@@ -1,6 +1,7 @@
 package com.mvc.controller;
-import org.springframework.stereotype.Controller;  
+import org.springframework.stereotype.Controller;   
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Hello {
@@ -14,5 +15,15 @@ public class Hello {
 	@ResponseBody()
 	public String welcome() {
 		return "Welcome to my Spring MVC";
+	}
+	
+	/*
+	 * 路徑: /sayhi?name=james&age=18
+	 */
+	@RequestMapping(value = "/sayhi")
+	@ResponseBody 
+	public String sayHi(@RequestParam(value = "name", defaultValue = "unKnown" ,required = false) String name,
+						@RequestParam("age") Integer age) {
+		return "Hello " + name + ", " + age;
 	}
 }
