eac5b4cfa6dc72c5229ef0f4ef412ed53f95a910
jamesliao <JamesCoding888@hotmail.com>
2022-05-30 19:21:59 +0800

    springmvc20220510

--- src/main/java/com/mvc/validator/ExamValidator.java
+++ src/main/java/com/mvc/validator/ExamValidator.java
@@ -1,5 +1,5 @@
 package com.mvc.validator;
-import org.springframework.stereotype.Component;  
+import org.springframework.stereotype.Component;   
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import com.mvc.entity.ExamTest;
